43da809aaf403006f3024237e87d89670d9467ca
MichaelAngeloMalicsi <michaelmalicsi090398@gmail.com>
2021-06-09 15:50:41 +0800

    po

---
+++ app/views/poapproval.py
@@ -0,0 +1,33 @@
+from rest_framework.views import APIView
+from app.models import PurchaseOrder, POItemsMerch
+from django import views
+from django.core.exceptions import NON_FIELD_ERRORS
+from django.db.models import query
+from django.http.response import JsonResponse
+from django.shortcuts import redirect, render, HttpResponse
+from django.views import View
+from ..forms import *
+from django.contrib import messages
+from django.contrib.auth import authenticate, login as auth_login, logout
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import never_cache
+from rest_framework import viewsets
+import sweetify
+
+class POapprovedView(View):
+    def get(self, request, format=None):
+
+        user = request.user
+        context = {
+            'purchase': user.branch.purchaseOrder.filter(approved=True),
+        }
+        return render(request, 'po-aprroved.html', context)
+
+class POnonapprovedView(View):
+    def get(self, request, format=None):
+
+        user = request.user
+        context = {
+            'purchase': user.branch.purchaseOrder.filter(approved=False),
+        }
+        return render(request, 'po-nonaprroved.html', context)
