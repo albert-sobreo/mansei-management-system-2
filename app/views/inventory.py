3fcc2176257ac9375edf961b3f7979e3d111e441
Jan Albert Sobreo <janalbertsobreo@gmail.com>
2021-06-14 14:35:01 +0800

    updates

--- app/views/inventory.py
+++ app/views/inventory.py
@@ -1,5 +1,5 @@
 from rest_framework.views import APIView
-from app.models import MerchandiseInventory
+from ..models import MerchandiseInventory
 from django import views
 from django.core.exceptions import NON_FIELD_ERRORS
 from django.db.models import query
@@ -13,6 +13,7 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import never_cache
 from rest_framework import viewsets
 import sweetify
+from decimal import Decimal
 
 class MerchInventoryView(View):
     def get(self, request, format=None):
@@ -27,15 +28,17 @@ class AddMerchInventoryAPI(APIView):
         a.code = request.data['code']
         a.classification = request.data['classification']
         a.type = request.data['type']
-        a.length = request.data['length']
-        a.width = request.data['width']
-        a.thickness = request.data['thickness']
+        a.length = Decimal(request.data['length'])
+        a.width = Decimal(request.data['width'])
+        a.thickness = Decimal(request.data['thickness'])
         a.purchasingPrice = request.data['purchasingPrice']
         a.sellingPrice = request.data['sellingPrice']
+        a.pricePerCubic = request.data['pricePerCubic']
         a.qtyT = 0
         a.qtyR = 0
         a.qtyA = 0
         a.um = "Per Piece"
+        a.vol = (a.width / 1000) * (a.length / 1000) * (a.thickness / 1000)
         a.totalCost = 0.0
         a.save()
         for warehouse in request.data['warehouse']:
