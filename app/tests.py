4b33b266f78efd36b30a76b5b908bcdc5b25219a
Jan Albert Sobreo <janalbertsobreo@gmail.com>
2022-03-17 00:42:20 +0800

    updates

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,6 @@
-from django.test import TestCase
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from time import sleep
 
-# Create your tests here.
